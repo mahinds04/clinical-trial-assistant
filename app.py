1f1759b9375ef344cff5bb416d5cea46b745224d
Mahin Das <mahinds04@gmail.com>
2025-08-25 14:37:19 +0530

    simplify: Remove non-essential imports and dependencies for initial deployment

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 import streamlit as st
 import pandas as pd
-import folium
-from streamlit_folium import folium_static
-import plotly.express as px
 from datetime import datetime, timedelta
 from pathlib import Path
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidfVectorizer
-import json
 import sys
 import os
 
